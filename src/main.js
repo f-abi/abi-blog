2cfb3d1893987e5b6cfa467de5965a5177526e27
f-abi <281327131@qq.com>
2020-11-28 23:14:08 +0800

    master

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ import store from "./store";
 //font and icon
 import "@/assets/fonts/font.css";
 import icon from "@/components/icon/AbiIcon.vue";
-
-//
+//cooike设置
 import { isCookie, getCookie } from "@/utility/cookie";
 if (isCookie("u_t")) {
   store.dispatch("logined", getCookie("u_t"));
